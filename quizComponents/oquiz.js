2a6829e66135bfd94d98de0fb7f7972074c40064
Ravika2410 <75975415+Ravika2410@users.noreply.github.com>
2022-09-14 16:41:32 +0530

    Update oquiz.js

--- quizComponents/oquiz.js
+++ quizComponents/oquiz.js
@@ -1,123 +1,74 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default class OQuiz extends Component {
-  constructor() {
-    super();
-    this.state = { icon: 'add-circle', color: 'grey', ques: '', ans: '' };
-  }
+export default class ChooseType extends Component {
   render() {
     return (
       <View>
-      
-        <Image
-          source={require('../assets/logo.png')}
-          style={{
-            alignSelf: 'center',
-            height: RFValue(200),
-            width: RFValue(200),
-            resizeMode: 'contain',
-            marginTop: RFValue(30),
-          }}
-        />
-        <Text>Write your first question here</Text>
-        <TextInput
-          style={{
-            height: RFValue(25),
-            width: RFValue(300),
-            backgroundColor: 'white',
-          }}
-          placeholder="Question here"
-          placeholderTextColor="black"
-        />
-        <Text>Write your options here</Text>
-        <TextInput
-          style={{
-            height: RFValue(25),
-            width: RFValue(300),
-            backgroundColor: 'white',
-          }}
-          placeholder="1."
-          placeholderTextColor="black"
-        />
-        <TextInput
-          style={{
-            height: RFValue(25),
-            width: RFValue(300),
-            backgroundColor: 'white',
-          }}
-          placeholder="2."
-          placeholderTextColor="black"
-        />
-        <TextInput
-          style={{
-            height: RFValue(25),
-            width: RFValue(300),
-            backgroundColor: 'white',
-          }}
-          placeholder="3."
-          placeholderTextColor="black"
-        />
-        <TextInput
-          style={{
-            height: RFValue(25),
-            width: RFValue(300),
-            backgroundColor: 'white',
-          }}
-          placeholder="4."
-          placeholderTextColor="black"
-        />
-        <Text>Write your answer here</Text>
-        <TextInput
-          style={{
-            height: RFValue(25),
-            width: RFValue(300),
-            backgroundColor: 'white',
-          }}
-          placeholder="Write you answer here"
-          placeholderTextColor="black"
-        />
+        <View>
+          <Image
+            source={require('../assets/logo.png')}
+            style={{
+              alignSelf: 'center',
+              height: RFValue(200),
+              width: RFValue(200),
+              resizeMode: 'contain',
+              marginTop: RFValue(30),
+            }}
+          />
+        </View>
         <TouchableOpacity
           style={{
-            height: RFValue(50),
-            width: RFValue(90),
-            backgroundColor: 'white',
-            marginTop: 80,
-          }} onPress={(a)=>{
-            this.setState({a:ques})
+            backgroundColor: '#55363E',
+            width: 160,
+            height: 80,
+            borderRadius: 20,
+            justifyContent: 'center',
+            marginTop: 100,
+            marginLeft: 90,
           }}>
-          <Ionicons
-          name={this.state.icon}
-          size={RFValue(40)}
-          color={this.state.color}
-          style={{ marginLeft: 40 }}
-        />
+          <Text
+            style={{
+              color: '#FFFFFF',
+              fontSize: 15,
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}>
+            Click to write subjective questions
+          </Text>
         </TouchableOpacity>
-       
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#55363E',
+            width: 160,
+            height: 80,
+            borderRadius: 20,
+            justifyContent: 'center',
+            marginTop: 50,
+            marginLeft: 90,
+          }}>
+          <Text
+            style={{
+              color: '#FFFFFF',
+              fontSize: 15,
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}>
+            Click to write objective questions
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: RFValue(1),
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: RFValue(24),
-    fontSize: RFValue(18),
+const st = StyleSheet.create({
+  text: {
+    margin: 10,
+    fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+    fontFamily: 'DynaPuff',
   },
 });
